bb39dbde2752dfe9afaf19d60a6981135e46c54a
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2022-01-31 18:48:19 -0300

    Header

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../../components/HeaderHome'
 
 import {} from './styled'
 
@@ -6,7 +7,8 @@ class Home extends Component {
     render() {
         return (
             <div>
-                Hello Home
+                <Header/>
+                
             </div>
         );
     }
