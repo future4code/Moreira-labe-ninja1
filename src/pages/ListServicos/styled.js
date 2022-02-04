e1ed4ee3b75e8f593193c5e125fc6093f12d0b05
HEINRICK7 <heinrickcostta@gmail.com>
2022-02-04 13:31:19 -0300

    update pages

--- src/pages/ListServicos/styled.js
+++ src/pages/ListServicos/styled.js
@@ -9,49 +9,43 @@ export const ContainerServicos = styled.div`
 
 export const Container = styled.div`
     margin: 50px auto;
-    width: 90%;
+    width: 95%;
     height: 100%;
-
     .container_search {
         height: 100px;
         align-items: center; 
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(300px, 4fr));
+        grid-template-columns: repeat(auto-fit, minmax(250px, 3fr));
         grid-gap: 20px;        
         justify-content: space-around;
     }
-
     .container_search input {
         border-radius: 8px;
         border: 1px solid #1DAE9D;
         padding:12px 24px;
     }
-
-    .container_list {
-        margin-top: 100px;
+    .container_search .select {
+        border-radius: 8px;
+        border: 1px solid #1DAE9D;
+        padding:12px 24px;
+        background-color: #FFFFFF;
     }
-
     .container_list {
-        list-style: none;
+        margin-top: 5px;
+        width: 90%;
+        padding: 100px;
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(400px, 3fr));
+        grid-template-columns: repeat(auto-fit, minmax(320px, 3fr));
         grid-gap: 20px;
-        padding: 0;
+        
     }
-
-    .container_list li {
-        /* background-color: #1DAE9D; */
-        padding: 30px;
-        color: #0000;
-        font-weight: bold;
-        /* box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px; */
-        border-radius: 8px;                
+    .container_list ul {
+        display: relative;
+         list-style: none;              
     }
-
     .buttons_card {
         margin-top: 20px;
     }
-
     .buttons_card button {
         padding: 12px 20px;
         border: 0;
@@ -61,7 +55,6 @@ export const Container = styled.div`
         background-color: #FFFFFF;
         box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
     }
-
     .card .price {
         margin: 10px;
         background-color: #FFFFFF;
@@ -70,7 +63,6 @@ export const Container = styled.div`
         border-radius: 8px;
         padding: 10px;
     }
-
     .container_cart {
         display: flex;
         background-color: #FFFFFF;
@@ -79,42 +71,45 @@ export const Container = styled.div`
         border-radius: 12px;
         flex-direction: column;
         justify-content: space-between;
-        margin-top: 110px;
+        margin: 110px 50px;
         box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
     }
-
+    .container_cartOff {
+        display: none;
+    }
     .header {
         border-bottom: 1px solid #1DAE9D;
         padding-left: 10px;
     }
-
     .header p {
         font-size: 24px;
         color: #1DAE9D;
     }
-
     .main {
         margin: 0 auto;
         width: 100%;
         height: 60vh;
         overflow-y:scroll;
     }
-
     .main ul {
         display: flex;
         flex-direction: column;
+        justify-content:center;
+        align-items: center;
         list-style: none;
         padding: 0;
     }
-
     .main li {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        padding: 30px;
+        padding: 20px;
+        background-color: #1DAE9D;
+        color: #FFFFFF;
+        width: 80%;
+        border-radius: 4px;
     }
-
     .footer {
         display: flex;
         background-color: #CCCCCC;
@@ -124,11 +119,9 @@ export const Container = styled.div`
         align-items: center;
         border-radius:0 0 12px 12px;
     }
-
     .footer p {
         font-weight: bold;
     }
-
     .footer button {
         background: #1DAE9D;
         color: #FFFFFF;
@@ -138,98 +131,109 @@ export const Container = styled.div`
         text-transform: uppercase;
         cursor: pointer;
     }
-
     .containerMainList {
         display: grid;
-        grid-template-columns: 3fr 1fr;
-        gap: 50px;               
+        grid-template-columns: 3fr 1fr;              
     }
-      .containerMainListOff {
+    .containerMainListOff {
         display: grid;
         grid-template-columns: 1fr;
-        gap: 50px;
-      }
-    /* .ContentAll {
-        transition: transform 1s;
-        transform-style: preserve-3d;
+       
     }
-    .card:hover .ContentAll {
-        transform: rotateY(180deg);
-        transition: transform 0,5s;
-    }
-    .Front,
-    .Back {
+    .content {
+    width:100%;
+    height:100%;
+    transition: transform 1s;
+    transform-style: preserve-3d;
+  }
+  
+  .card:hover .content {
+    transform: rotateY( 180deg ) ;
+    transition: transform 0.5s;
+    
+  }
+  .card {
+    position: relative;
+    margin: 100px 0 0 0;
+    height: 42vh;
+    width: auto;
+
+  }
+  .front,
+  .back {
     position: absolute;
-    height: 100%;
+    top: -100px;
+    left:-40px;
     width: 100%;
     height: 100%;
-    color: #FFFFFF;
+    color: #ffffff;
     text-align: center;
-    font-size: 20px;
     border-radius: 10px;
     backface-visibility: hidden;
   }
-    .Back {
-    background: #03446A;
-    color: white;
-    transform: rotateY( 180deg );
-  } */
-
-  .card {
-  width: 450px;
-  height: 300px;
-  perspective: 1000px;
-  padding: 20px;
-
-}
-
-.ContentAll {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.6s;
-  transform-style: preserve-3d;
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-}
-
-.card:hover .ContentAll {
-  transform: rotateY(180deg);
-}
-
-.Front, .Back {
-  position: absolute;
-    
-    height: 100%;
+  
+  .front {
+    display: flex;
+    background-color: #1DAE9D;
+    padding: 20px;
     width: 100%;
     height: 100%;
     color: #FFFFFF;
-    text-align: center;
     font-size: 20px;
-    border-radius: 10px;
-    -webkit-backface-visibility: hidden; 
-    backface-visibility: hidden;
-}
+    font-weight: bold;
 
-.Front {
-  background-color: #1DAE9D;
-  color: black;
-  padding: 20px;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+   
 }
-
-.Back {
+.front div {
+    padding: 10px;
+}
+.back {
     position: absolute;
+    top: -100px;
+    left:10px;
     background-color: #1DAE9D;
     height: 100%;
     width: 100%;
-    height: 100%;
     color: #FFFFFF;
     text-align: center;
-    font-size: 20px;
+    font-size: 14px;
+    font-weight: bold;
     padding: 20px;
     border-radius: 10px;
-  transform: rotateY(180deg);
+    transform: rotateY(180deg);
+}
+.typePayments {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-size: 14px;
+
+    justify-content: space-between;
+    margin: 20px auto;
+    font-size: 14px;
+    flex-wrap: wrap;
+}
+.typePayments ul {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    background-color:#FFFFFF;
+    padding: 4px;
+    color:#1DAE9D;
+    border-radius: 4px;
+
+}
+.typePayments li {
+    background-color:#FFFFFF;
+    padding: 4px;
+    color:#1DAE9D;
+    border-radius: 4px;
+
+}
+.back .price {
+    font-size: 24px;
 }
 `;
-
-
